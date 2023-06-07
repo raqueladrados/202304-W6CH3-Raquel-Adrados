dea35017c17f69619db4796519226c47f458f4cb
Raquel Adrados <radrados00@gmail.com>
2023-06-07 21:17:01 +0200

    Add actions creator of chracters

--- src/redux/actions.creator.ts
+++ src/redux/actions.creator.ts
@@ -1,8 +1,22 @@
 import { actionsTypes } from "./actions.types";
 import { CharactersData } from "../models/characters.model";
+import { createAction } from "@reduxjs/toolkit";
 type Keys = keyof typeof actionsTypes;
 
 export type CharactersAction = {
   type: (typeof actionsTypes)[Keys];
   payload: CharactersData[];
 };
+
+export const communicateCharactersAction = createAction<CharacterData[]>(
+  actionsTypes.communicateAction
+);
+export const diedCharactersAction = createAction<CharactersData[]>(
+  actionsTypes.diedAction
+);
+export const updateCharactersAction = createAction<CharactersData[]>(
+  actionsTypes.updateCharacters
+);
+export const loadCharactersAction = createAction<CharactersData[]>(
+  actionsTypes.loadCharacters
+);
