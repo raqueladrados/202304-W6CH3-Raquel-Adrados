1fd6298fd1d9cd15ce8e15cf4831fd006df26a97
Raquel Adrados <radrados00@gmail.com>
2023-06-07 20:36:33 +0200

    Add store file and folder and the function

---
+++ src/core/store/store.ts
@@ -0,0 +1,16 @@
+import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    characters: charactersReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
