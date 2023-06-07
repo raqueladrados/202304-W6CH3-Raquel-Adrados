9bb3514b5af0022c3c45137407ca601a41f3081f
Raquel Adrados <radrados00@gmail.com>
2023-06-07 20:38:04 +0200

    Add provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./core/App.tsx";
 import "./index.css";
+import { store } from "./core/store/store.ts";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
